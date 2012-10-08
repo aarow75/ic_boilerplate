93557fc1a64684c4a6c384a94c205b89eb2a1b5e
Aaron Swensen <aaron.swensen@gmail.com>
2012-10-08 15:02:14 -0600

    Added default page and sample json data

---
+++ data.js
@@ -0,0 +1,78 @@
+IC._volume = {
+header: [
+{value:"Volume data",attributes:{"class":"essential"}},
+{value:"2012 Totals",attributes:{"class":"essential"}},
+{value:"Aug 2012",attributes:{"class":"essential"}},
+{value:"July 2012"},
+{value:"June 2012"},
+{value:"2011 Totals"}
+],
+content: [
+[{value:"Personal Volume",attributes:{"class":"essential"}},{value:"20,242",attributes:{"class":"essential"}},{value:"3,016",attributes:{"class":"essential"}},{value:"3,538"},{value:"4,118"},{value:"-27,550"}],
+[{value:"Group Volume",attributes:{"class":"essential"}},{value:"40,516,103",attributes:{"class":"essential"}},{value:"2,765,730",attributes:{"class":"essential"}},{value:"2,161,486"},{value:"9,795,968"},{value:"64,514,357"}],
+[{value:"Total Volume",attributes:{"class":"essential"}},{value:"40,536,345",attributes:{"class":"essential"}},{value:"2,768,746",attributes:{"class":"essential"}},{value:"2,165,024"},{value:"9,800,086"},{value:"64,486,807"}],
+[{value:"New Customers",attributes:{"class":"essential"}},{value:"550",attributes:{"class":"essential"}},{value:"41",attributes:{"class":"essential"}},{value:"9"},{value:"0"},{value:"0"}],
+[{value:"New Distributors",attributes:{"class":"essential"}},{value:"50",attributes:{"class":"essential"}},{value:"41",attributes:{"class":"essential"}},{value:"9"},{value:"0"},{value:"10"}],
+[{value:"New Group Distributors"},{value:"550"},{value:"432"},{value:"117"},{value:"0"},{value:"10"}],
+[{value:"Total Recruits",attributes:{"class":"essential"}},{value:"600",attributes:{"class":"essential"}},{value:"473",attributes:{"class":"essential"}},{value:"126"},{value:"0"},{value:"20"}],
+[{value:"Customer Orders",attributes:{"class":"essential"}},{value:"0",attributes:{"class":"essential"}},{value:"0",attributes:{"class":"essential"}},{value:"0"},{value:"0"},{value:"0"}],
+[{value:"Distributor Orders",attributes:{"class":"essential"}},{value:"110",attributes:{"class":"essential"}},{value:"45",attributes:{"class":"essential"}},{value:"43"},{value:"22"},{value:"0"}],
+[{value:"Total Orders",attributes:{"class":"essential"}},{value:"110",attributes:{"class":"essential"}},{value:"45",attributes:{"class":"essential"}},{value:"43"},{value:"22"},{value:"0"}]
+],
+footer: []
+}
+
+IC._alerts = {
+title: "Alerts",
+items: [
+{text:"You have 457 new Distributors in your downline", attributes:{href:"#"}},
+{text:"You have 43 new Customers", attributes:{href:"#"}},
+{text:"You have 55 that are first time sponsors in your downline", attributes:{href:"#"}},
+{text:"You have 30 distributor(s) whose anniversaries are within the next 30 days.", attributes:{href:"#"}},
+{text:"You have 93 distributor(s) whose birthdays are within the next 30 days.", attributes:{href:"#"}},
+{text:"You have 32 distributors that have not placed an order.", attributes:{href:"#"}}
+]
+}
+
+IC._reports = {
+title: "Reports",
+items: [
+{text:"Customer Anniversary", attributes:{href:"#"}},
+{text:"Customer Search", attributes:{href:"#"}},
+{text:"Autoship", attributes:{href:"#"}},
+{text:"Birthday", attributes:{href:"#"}},
+{text:"Geographic Search", attributes:{href:"#"}},
+{text:"New Customer", attributes:{href:"#"}},
+{text:"New Distributor", attributes:{href:"#"}},
+{text:"Distributor Search", attributes:{href:"#"}},
+{text:"Volume", attributes:{href:"#"}},
+{text:"Orders", attributes:{href:"#"}},
+{text:"No Order", attributes:{href:"#"}},
+{text:"Close to Title", attributes:{href:"#"}},
+{text:"Close To Check", attributes:{href:"#"}},
+{text:"Rank Advancements", attributes:{href:"#"}},
+{text:"Renewals", attributes:{href:"#"}},
+{text:"Customer Orders", attributes:{href:"#"}},
+{text:"Genealogy", attributes:{href:"#"}}
+]
+}
+
+IC._saved_reports = {
+title:"Saved reports",
+items:[
+{text:"April promotions", attributes:{href:"#"}},
+{text:"January renewals", attributes:{href:"#"}},
+{text:"January birthdays", attributes:{href:"#"}},
+{text:"customer search", attributes:{href:"#"}},
+{text:"first time sponsor", attributes:{href:"#"}}
+]
+}
+
+IC._page_nav = {
+	items:[
+	{text:"<img src=\"images/112-group.png\" /><br />Volumes",attributes:{href:"#","class":"volumes"}},
+	{text:"<img src=\"images/124-bullhorn.png\" /><br />Alerts",attributes:{href:"#","class":"alerts"}},
+	{text:"<img src=\"images/179-notepad.png\" /><br />Reports",attributes:{href:"#","class":"reports"}},
+	{text:"<img src=\"images/81-dashboard.png\" /><br />Saved Reports",attributes:{href:"#","class":"saved-reports"}}
+	]
+}
